3f9134ea0dc2b76b3055f4a6491d855a26992436
Joe Albrecht <joedevpro@gmail.com>
2024-09-05 18:07:13 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+export const App = () => {
+
+
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind with Vite!</h1>
+    </div>
+    </>
+  )
+}
+
